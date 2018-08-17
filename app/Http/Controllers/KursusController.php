462b1363caa29e909c630cdb6ed32e1245a75576
Bilik Latihan BPM KBS <lab_kbs@kbs.local>
2018-08-17 10:41:10 +0800

    model and controller

---
+++ app/Http/Controllers/KursusController.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Http\Controllers;
+
+class KursusController extends Controller {
+	function listing() {
+
+	}
+
+	function add() {
+
+	}
+
+	function save() {
+
+	}
+
+	function delete($id) {
+
+	}
+
+	function edit($id) {
+
+	}
+}
